099b7a59665cf946efab73393c472a2ed4f29640
Sahar Nafisi <sahar.nafisi95@gmail.com>
2017-08-07 23:10:11 +0430

    post method added

--- tornadoTest.py
+++ tornadoTest.py
@@ -3,12 +3,15 @@ import tornado.ioloop
 
 class mainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("hello world")
+        self.write("get request send")
+    
+    def post(self):
+        self.write("post request send")
 
 
 
 
 if __name__=="__main__":
-    app=tornado.web.Application([(r"/main",mainHandler)])
+    app=tornado.web.Application([(r"/",mainHandler)])
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
