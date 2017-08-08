0b758f61d795d068fabc0dddc106e616f07262a6
Sahar Nafisi <sahar.nafisi95@gmail.com>
2017-08-08 20:30:17 +0430

    post changed

--- tornadoTest.py
+++ tornadoTest.py
@@ -1,19 +1,30 @@
 import tornado.web
 import tornado.ioloop
+import os
 
-class mainHandler(tornado.web.RequestHandler):
+
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("get request send")
-    
+
     def post(self):
         #self.write("post request send")
-        items=["صفحه اصلی","ثبت نام","ورود","درباره ما","خروج"]
-        #items=["sahar"]
-        name="sahar"
-        self.render("header.html" ,items=items, name=name)
+        items = ["صفحه اصلی", "ثبت نام", "ورود", "درباره ما", "خروج"]
+        # items=["sahar"]
+        name = "sahar"
+        self.render("header.html", items=items, name=name)
 
 
-if __name__=="__main__":
-    app=tornado.web.Application([(r"/",mainHandler)])
+if __name__ == "__main__":
+    settings = {
+        "static_path": os.path.join(os.path.dirname(__file__), "static")
+    }
+
+    app = tornado.web.Application([
+        (r"/", MainHandler,
+        dict(path=settings['static_path']),
+    ], **settings)
+
+
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
