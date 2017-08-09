ba5921601252e49eabf64806a57ae6e0ec4ff46c
Sahar Nafisi <sahar.nafisi95@gmail.com>
2017-08-09 23:08:28 +0430

    register user added

--- tornadoTest.py
+++ tornadoTest.py
@@ -1,20 +1,41 @@
 import tornado.web
 import tornado.ioloop
 import os
+import sqlite3
+import re
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         #self.write("get request send")
         items = ["صفحه اصلی", "ثبت نام", "ورود", "درباره ما", "خروج"]
-        self.render("header.html",items=items)
+        self.render("header.html", items=items)
 
     def post(self):
         #self.write("post request send")
         items = ["صفحه اصلی", "ثبت نام", "ورود", "درباره ما", "خروج"]
         # items=["sahar"]
-        
 
+
+class RegisterUser(tornado.web.RequestHandler):
+    def get(self):
+        self.render("Register.html")
+
+    def post(self):
+        fname = self.get_argument("fname")
+        lname = self.get_argument("lname")
+        education = self.get_argument("education")
+        email = self.get_argument("email")
+        password = self.get_argument("password")
+        key=self.get_argument("key")
+        key_rex=r'[a-z]{2}[0-9]{3}'
+        query = """INSERT INTO 'user'('firstName','lastName','education','email','password','keys')
+         VALUES(?,?,?,?,?,?)"""
+        cursor = self.application.db.cursor()
+        #cursor.execute(query, [fname, lastname, education, email, password, "sssss"])
+        cursor.execute(query, ["sahar", "nafisi", "diplom", "sahar@gmail.com", "123", "sssss"])
+        self.application.db.commit()
+        self.redirect("/addUser")
 
 if __name__ == "__main__":
     settings = {
@@ -23,8 +44,9 @@ if __name__ == "__main__":
 
     app = tornado.web.Application([
         (r"/", MainHandler),
+        (r"/addUser", RegisterUser),
     ], **settings)
 
-
+    app.db = sqlite3.connect("site.db")
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
---
+++ user.sql
@@ -0,0 +1,10 @@
+BEGIN TRANSACTION;
+CREATE TABLE `user` (
+	`firstName`	VARCHAR(10),
+	`lastName`	VARCHAR(20),
+	`education`	VARCHAR(15),
+	`email`	VARCHAR(50),
+	`password`	VARCHAR(20),
+	`keys`	VARCHAR(5)
+);
+COMMIT;
